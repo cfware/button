94b68d04077cdc1e03cc9f8d4955dd5f7dc21b02
Corey Farrell <git@cfware.com>
2022-08-27 08:33:24 -0400

    fix!: Update dependencies

--- button.js
+++ button.js
@@ -1,85 +1,86 @@
-import ShadowElement, {html, template, define, reflectBooleanProperties, reflectStringProperties} from '@cfware/shadow-element';
+import ShadowElement, {html, template, css, adoptedStyleSheets, define, reflectBooleanProperties, reflectStringProperties} from '@cfware/shadow-element';
 import {immediateBlockEvent} from '@cfware/event-blocker';
 
 import '@cfware-app/icon';
 
 class CFWareButton extends ShadowElement {
-	_setTabIndex() {
-		if (this.noTab) {
-			this.setAttribute('tabindex', -1);
-		} else if (this.disabled) {
-			this.removeAttribute('tabindex');
-		} else {
-			this.setAttribute('tabindex', 0);
-		}
-	}
+    _setTabIndex() {
+        if (this.noTab) {
+            this.setAttribute('tabindex', -1);
+        } else if (this.disabled) {
+            this.removeAttribute('tabindex');
+        } else {
+            this.setAttribute('tabindex', 0);
+        }
+    }
 
-	constructor() {
-		super();
+    constructor() {
+        super();
 
-		const clickKeys = new Set(['Enter', ' ']);
-		this.addEventListener('keypress', event => {
-			if (clickKeys.has(event.key)) {
-				this.click();
-			}
-		});
+        const clickKeys = new Set(['Enter', ' ']);
+        this.addEventListener('keypress', event => {
+            if (clickKeys.has(event.key)) {
+                this.click();
+            }
+        });
 
-		this.addEventListener('click', event => {
-			if (this.disabled) {
-				immediateBlockEvent(event);
-			}
-		}, {capture: true});
+        this.addEventListener('click', event => {
+            if (this.disabled) {
+                immediateBlockEvent(event);
+            }
+        }, {capture: true});
 
-		this.setAttribute('role', 'button');
-		this._setTabIndex();
-	}
+        this.setAttribute('role', 'button');
+        this._setTabIndex();
+    }
 
-	get [template]() {
-		this._setTabIndex();
-		return html`
-			<style>
-				:host {
-					display: inline-block;
-					position: relative;
-					background: var(--background, inherit);
-					color: var(--color, #000c);
-					cursor: pointer;
-					outline: 0;
-					padding: .5em 0;
-					width: 2em;
-					text-align: center;
-					line-height: 1;
-				}
+    static [adoptedStyleSheets] = [
+        css`
+            :host {
+                display: inline-block;
+                position: relative;
+                background: var(--background, inherit);
+                color: var(--color, #000c);
+                cursor: pointer;
+                outline: 0;
+                padding: .5em 0;
+                width: 2em;
+                text-align: center;
+                line-height: 1;
+            }
 
-				:host([spacer]) {
-					visibility: hidden;
-				}
+            :host([spacer]) {
+                visibility: hidden;
+            }
 
-				:host([disabled]), :host([disabled]) * {
-					cursor: default;
-				}
+            :host([disabled]), :host([disabled]) * {
+                cursor: default;
+            }
 
-				:host(:not([disabled]):hover) {
-					background: var(--hover-background, inherit);
-					color: var(--hover-color, #005d90);
-				}
+            :host(:not([disabled]):hover) {
+                background: var(--hover-background, inherit);
+                color: var(--hover-color, #005d90);
+            }
 
-				:host(:not([disabled]):focus) {
-					background: var(--focus-background, #bbf4);
-					color: var(--focus-color, inherit);
-				}
+            :host(:not([disabled]):focus) {
+                background: var(--focus-background, #bbf4);
+                color: var(--focus-color, inherit);
+            }
 
-				:host([disabled]) cfware-icon {
-					opacity: 0.25;
-				}
+            :host([disabled]) cfware-icon {
+                opacity: 0.25;
+            }
 
-				cfware-icon {
-					cursor: pointer;
-				}
-			</style>
-			<cfware-icon icon=${this.icon} />
-		`;
-	}
+            cfware-icon {
+                cursor: pointer;
+            }
+        `
+    ];
+
+    get [template]() {
+        this._setTabIndex();
+        return html`<cfware-icon icon=${this.icon} />`;
+    }
 }
 
 reflectBooleanProperties(CFWareButton, ['disabled', 'noTab']);
